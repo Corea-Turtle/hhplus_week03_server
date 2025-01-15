3854d7b8d1fce2b3ccb94037b41a39fdb7bb02c7
Corea-Turtle <eastino@gmail.com>
2025-01-15 16:35:14 +0900

    [feat] 잔액 충전/조회 api 구현/테스트(실패 테스트 추가 필요)

--- src/main/java/kr/hhplus/be/server/interfaces/api/controller/user/UserController.java
+++ src/main/java/kr/hhplus/be/server/interfaces/api/controller/user/UserController.java
@@ -1,20 +1,28 @@
 package kr.hhplus.be.server.interfaces.api.controller.user;
 
 
+import kr.hhplus.be.server.domain.user.UserService;
+import kr.hhplus.be.server.interfaces.dto.user.request.UserPointUpdateRequest;
+import kr.hhplus.be.server.interfaces.dto.user.response.UserPointResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("api/user")
 public class UserController {
 
+    private final UserService userService;
 
     //(포인트) 잔액 조회
-    //@GetMapping("/{userId}/point")
-
-
+    @GetMapping("/{userId}/point")
+    public long getCurrentPointAmount(@RequestBody UserPointResponse response){
+       return userService.getCurrentPointAmount(response);
+    }
 
     //(포인트) 충전
-    //@PostMapping("/point/charge")
-
-
+    @PostMapping("/point/charge")
+    public void updatePointAmountById(@RequestBody UserPointUpdateRequest request){
+        userService.updatePointAmountById(request);
+    }
 }
--- src/test/java/kr/hhplus/be/server/domain/user/UserServiceTest.java
+++ src/test/java/kr/hhplus/be/server/domain/user/UserServiceTest.java
@@ -2,6 +2,7 @@ package kr.hhplus.be.server.domain.user;
 
 import jakarta.transaction.Transactional;
 import kr.hhplus.be.server.infrastructure.user.UserJpaRepository;
+import kr.hhplus.be.server.interfaces.dto.user.request.UserPointUpdateRequest;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -29,10 +30,54 @@ class UserServiceTest {
         userJpaRepository.save(user);
 
         //when
-        User userFinded = userJpaRepository.findById(user.getUserId())
+        User userFound = userJpaRepository.findById(user.getUserId())
                 .orElseThrow(()->new IllegalArgumentException("유저가 존재하지 않습니다."));
 
         //then
-        Assertions.assertThat(userFinded.getPointAmount()).isEqualTo(10);
+        Assertions.assertThat(userFound.getPointAmount()).isEqualTo(10);
+    }
+
+    @DisplayName("유저아이디를 조회한 후 해당 유저 아이디의 포인트를 더한 값으로 업데이트한다.")
+    @Test
+    void updatePointPositiveAmountById() {
+        //given
+        User user = new User("이유저", 10);
+        userJpaRepository.save(user);
+
+        UserPointUpdateRequest request = new UserPointUpdateRequest(user.getUserId(), 100, true);
+
+        long currentPoint = user.getPointAmount();
+        int updatePoint = request.getUpdateAmount();
+
+        //when
+        User userFound = userJpaRepository.findById(user.getUserId())
+                .orElseThrow(()->new IllegalArgumentException("유저가 존재하지 않습니다."));
+
+        userFound.updatePoint(currentPoint+updatePoint);
+
+        Assertions.assertThat(userFound.getPointAmount()).isEqualTo(110);
+        //then
+    }
+
+    @DisplayName("유저아이디를 조회한 후 해당 유저 아이디의 포인트를 뺸 값으로 업데이트한다.")
+    @Test
+    void updatePointNegativeAmountById() {
+        //given
+        User user = new User("이유저", 100);
+        userJpaRepository.save(user);
+
+        UserPointUpdateRequest request = new UserPointUpdateRequest(user.getUserId(), 90, false);
+
+        long currentPoint = user.getPointAmount();
+        int updatePoint = request.getUpdateAmount();
+
+        //when
+        User userFound = userJpaRepository.findById(user.getUserId())
+                .orElseThrow(()->new IllegalArgumentException("유저가 존재하지 않습니다."));
+
+        userFound.updatePoint(currentPoint-updatePoint);
+
+        Assertions.assertThat(userFound.getPointAmount()).isEqualTo(10);
+        //then
     }
 }
